f2877313830516fad41cb37cbfc2b5e07dd4c255
louis <liuyichao1207@gmail.com>
2020-05-01 00:11:09 +0900

    icon components add

--- src/components/icon/icon.stories.tsx
+++ src/components/icon/icon.stories.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import Icon from './icon';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
-import { ColorEnum } from '../common/ColorEnum';
+import { TextColorEnum } from '../common/ColorEnum';
 import { CodeBox } from '../codeBox/codeBox';
 
 export default { title: 'Icon', component: Icon };
@@ -9,7 +9,7 @@ export default { title: 'Icon', component: Icon };
 export const Basic: FC = () => {
     return (
         <CodeBox title={'基本使用'} Desc={'基本使用'}>
-            <Icon IconDefinition={faEdit} Color={ColorEnum.info}></Icon>
+            <Icon IconDefinition={faEdit} Color={TextColorEnum.info}></Icon>
         </CodeBox>
 
     );
--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ColorEnum } from "../common/ColorEnum";
+import { TextColorEnum } from "../common/ColorEnum";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import clsx from "clsx";
@@ -8,7 +8,7 @@ import './styles.scss';
 
 export interface IconProps extends React.Props<HTMLSpanElement> {
     IconDefinition: IconDefinition,
-    Color?: ColorEnum
+    Color?: TextColorEnum
 }
 
 const Icon = React.forwardRef((props: IconProps, ref?: React.Ref<HTMLSpanElement>) => {
